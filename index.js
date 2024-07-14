f3b1ddb322db2218a15e94738938c97958f3de5f
Iamouma <stephenouma452@gmail.com>
2024-07-14 12:04:16 +0300

    Minor changes

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const app = express();
 const port = 3000;
 const registerRouter = require('./routes/register');
 const authRouter = require('./routes/auth');
-const expensesRouter = require('./routes/expenses');
-
 
 // Serve static files for the public directory
 app.use(express.static('public'));
@@ -17,7 +15,6 @@ app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 // API routes
 app.use('/api/register', registerRouter);
 app.use('/api/auth', authRouter);
-app.use('/api/expenses', expensesRouter);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
