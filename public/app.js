b73d8f3f69cc770e474bb0394a45f71991d65ebb
Iamouma <stephenouma452@gmail.com>
2024-07-30 21:24:27 +0300

    More files

--- public/app.js
+++ public/app.js
@@ -85,3 +85,81 @@ function logout() {
     // Redirect to the login page
     window.location.href = 'login.html';
 }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const editExpenseForm = document.getElementById('editExpenseForm');
+    const successMessage = document.getElementById('successMessage');
+    const urlParams = new URLSearchParams(window.location.search);
+    const expenseId = urlParams.get('id');
+
+    // Fetch expense details and populate the form
+    async function fetchExpenseDetails() {
+        const token = localStorage.getItem('token');
+
+        try {
+            const response = await fetch(`/api/expenses/${expenseId}`, {
+                headers: {
+                    'Authorization': token
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch expense details');
+            }
+
+            const expense = await response.json();
+
+            document.getElementById('expenseDate').value = expense.date.split('T')[0];
+            document.getElementById('category').value = expense.category;
+            document.getElementById('amount').value = expense.amount;
+            document.getElementById('description').value = expense.description;
+        } catch (error) {
+            console.error('Error fetching expense details:', error);
+        }
+    }
+
+    fetchExpenseDetails();
+
+    // Update expense
+    editExpenseForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(editExpenseForm);
+        const updatedExpenseData = {
+            expenseDate: formData.get('expenseDate'),
+            category: formData.get('category'),
+            amount: formData.get('amount'),
+            description: formData.get('description')
+        };
+
+        const token = localStorage.getItem('token');
+        try {
+            const response = await fetch(`/api/expenses/${expenseId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': token
+                },
+                body: JSON.stringify(updatedExpenseData)
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to update expense');
+            }
+
+            // Show success message
+            successMessage.style.display = 'block';
+            setTimeout(() => {
+                successMessage.style.display = 'none';
+            }, 3000);
+        } catch (error) {
+            console.error('Error updating expense:', error);
+        }
+    });
+
+    function logout() {
+        localStorage.removeItem('token');
+        window.location.href = 'login.html';
+    }
+});
